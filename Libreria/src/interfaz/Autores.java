8e34f59fb49e68d5eb5875aeee95f111a3e4f0b8
AnthonyMonc <antho_monca@hotmail.com>
2020-02-17 18:58:58 -0500

    tabla

--- Libreria/src/interfaz/Autores.java
+++ Libreria/src/interfaz/Autores.java
@@ -76,9 +76,16 @@ Metodos_autores met = new Metodos_autores();
             Class[] types = new Class [] {
                 java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class, java.lang.String.class
             };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false
+            };
 
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
             }
         });
         jScrollPane1.setViewportView(tbl_autoresU);
