de4582b32368fc28471ef6a1b48623be4cd2d17e
AnthonyMonc <antho_monca@hotmail.com>
2021-07-07 21:10:55 -0500

    form

--- Practica_3/src/componentes/MyForm2.js
+++ Practica_3/src/componentes/MyForm2.js
@@ -1,32 +1,38 @@
+//my form2 tambien es un componente por eso importamos react
 import React from 'react';
 
 //mi elemento que voy a utilizar
 //todo elemento debe tener un render con un return
 class MyForm2  extends React.Component{
+    //debe ser  el mismo nombre que el de los input 
     state={
         color:'',
         nombre:''
     }
     //en este evento estoy tomando el evento y lo imprimo 
     //puedo ponerle cualquier nombre en ese caso myOnSubmit
-    myOnSubmit=(evn)=>{
-        console.log(evn)
+    myOnSubmit=(evn)=>{                
         evn.preventDefault();
+        console.log(this.state)
     }
-
+    //el evento puede tener cualquier nombre
+    //usamos name para poder traer el name del input
+    //captura cada que tecleamo
     myOnchange=(env)=>{
-        console.log("myOnchange",env.target.value);
+        console.log("myOnchange",env.target.value, "name:",env.name);
         this.setState({
-            color:env.target.value
+            [env.target.name]:env.target.value
         })
     }
+
     render(){
         return(
             <form onSubmit={this.myOnSubmit}>
-                <input type="text" placeholder="Ingrese el Color" onChange={this.myOnchange} value={this.state.color}></input>
+                
+                <input name="color" type="text" placeholder="Ingrese el Color" onChange={this.myOnchange} value={this.state.color}></input>
                 <br/>
                 <br/>
-                <input type="text" placeholder="Ingrese el nombre de la imagen" onChange={this.myOnchange}  value={this.state.color}></input>
+                <input name="nombre" type="text" placeholder="Ingrese el nombre de la imagen" onChange={this.myOnchange}  value={this.state.nombre}></input>
                 <br/>
                 <br/>
                 <input type="submit"></input>
